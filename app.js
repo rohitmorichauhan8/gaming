03d99ac5fec2d57e18cf1ec180eba3de752a34db
rohitmorichauhan8 <rohitmorichauhan8@gmail.com>
2026-02-15 11:13:42 +0530

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const video=document.querySelector('.hero-video');
 const movieList=['video/hero-1.mp4','video/hero-2.mp4','video/hero-3.mp4','video/hero-4.mp4'];
 
 let index=0;
-nextButton.addEventListener('click',function()){
+nextButton.addEventListener('click',function(){
 
     index +=1
     video.src=movieList[index];
@@ -12,4 +12,4 @@ nextButton.addEventListener('click',function()){
     if(index===3){
     index=-1;
     }
-}
+})
